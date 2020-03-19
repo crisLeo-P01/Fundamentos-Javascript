c5336fce12dfc1f41cdede8dc88072e2c814b4e9
crisLeo <cristianleopaiva@gmail.com>
2020-03-19 00:01:00 -0300

    Array - Métodos & Spread Operator

---
+++ 05 - Arrays/script02.js
@@ -0,0 +1,12 @@
+let numeros = [1,2,3,4,5,6,7];
+
+console.log(numeros.length);
+console.log(numeros[3]);
+console.log(`La suma de ${numeros[2]} y ${numeros[4]} es ` + (numeros[2] + numeros[4]));
+
+////////////////////////////////////////////
+
+let palabras = ['Hola' , 'estamos' , 'trabajando' , 'con' , 'SublimeText3'];
+
+console.log(`${palabras[1]} ${palabras[2]}`);
+console.log(`La palabra "${palabras[2]}" tiene ${palabras[2].length} letras`);
