cc2d81295179e78f7e9abb94f5854f877fabaa18
crisLeo <cristianleopaiva@gmail.com>
2020-03-22 13:23:53 -0300

    Async / avait

--- 19 - Async - await/script01.js
+++ 19 - Async - await/script01.js
@@ -22,7 +22,6 @@ sayHello().then(res => console.log(res));
 */
 
 const users = [{id: 1, name: 'Dorian'}, {id: 2, name: 'Laura'}, {id: 3,name: 'Carlos'}];
-
 const emails = [{id: 1,email: 'dorian@gmail.com'},{id: 2,email: 'laura@gmail.com'}];
 
 const getUser = async (id) => {
