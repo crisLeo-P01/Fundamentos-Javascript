95be28cb555f6f324028d29b789e3a4b70bf6951
crisLeo <cristianleopaiva@gmail.com>
2020-03-22 16:48:48 -0300

    Axios - GET & POST

---
+++ 20 - Librería axios - GET & POST/script01.js
@@ -0,0 +1,43 @@
+/*
+Librería axios
+Axios es una librería basado en promesas para el cliente
+y el servidor
+https://desarrolloweb.com/articulos/axios-ajax-cliente-http-javascript.html
+ */
+
+const button = document.getElementById('button');
+
+//res = response = respuesta
+/*
+ button.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+        .then(res => res.json())
+        .then(res => {
+            const list = document.getElementById('list');
+            const fragment = document.createDocumentFragment();
+            for (const userInfo of res) {
+                const listItem = document.createElement('LI');
+                listItem.textContent = `${userInfo.id} - ${userInfo.name}`;
+                fragment.appendChild(listItem)
+            }
+            list.appendChild(fragment)
+        })
+});
+*/
+
+button.addEventListener('click', () => {
+  axios ({
+    method: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/users'
+  }).then(res => {
+    const list = document.getElementById('list');
+    const fragment = document.createDocumentFragment();
+    for (const userInfo of res.data) {
+      const listItem = document.createElement('LI');
+      listItem.textContent = `${userInfo.id} - ${userInfo.name}`;
+      fragment.appendChild(listItem)
+    }
+    list.appendChild(fragment)
+  }).catch(err => console.log(err))
+});
