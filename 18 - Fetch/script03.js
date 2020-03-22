7eaf2592b1a10b461cd93d0e6e8b2babef17a2b0
crisLeo <cristianleopaiva@gmail.com>
2020-03-22 12:56:31 -0300

    Fetch: Lectura de archivos

--- 18 - Fetch/script03.js
+++ 18 - Fetch/script03.js
@@ -1,8 +1,16 @@
+/*
+    Fetch API
+        blob(): Binary Long OBject
+        Si ponemos la ruta hacia el archivo podemos leer ese archivo y renderizarlo en pantalla
+        URL: Con el objeto URL usando el método createObjectURL(archivo) podemos crear una ruta válida para ver ese archivo
+        https://developer.mozilla.org/es/docs/Web/API/URL
+*/
+
 const buttonImg = document.getElementById('button-img');
 const buttonPDF = document.getElementById('button-pdf');
 
 buttonImg.addEventListener('click', () => {
-  fetch('../assets/images/dog.jpg')
+  fetch('../assets/images/dog.jpg') //Petición a un recurso
     .then(res => res.blob())
     .then(img => {
       document.getElementById('img').src = URL.createObjectURL(img);
