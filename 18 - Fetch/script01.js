5a6d61a64508e80ad81dd2f11f9f315d00822f5b
crisLeo <cristianleopaiva@gmail.com>
2020-03-23 00:00:36 -0300

    Fetch Introducción

--- 18 - Fetch/script01.js
+++ 18 - Fetch/script01.js
@@ -21,7 +21,7 @@ const button = document.getElementById('button');
 //res = response = respuesta
 button.addEventListener('click', () => {
   fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.ok == true ? Promise.resolve(res) : Promise.reject(res))
+    .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res)) // res.ok -> res.og == true
 
     .then(res => res.json())
     .then(res => {
