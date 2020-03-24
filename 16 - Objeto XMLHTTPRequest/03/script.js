ca73c445304b112cee16af8379fe21b0a476a270
crisLeo <cristianleopaiva@gmail.com>
2020-03-24 16:27:44 -0300

    Objeto XMLHTTPRequest

--- 16 - Objeto XMLHTTPRequest/03/script.js
+++ 16 - Objeto XMLHTTPRequest/03/script.js
@@ -1,16 +1,16 @@
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); //De está forma se evita recargar la página
-  sendData(form);
+    e.preventDefault(); //De está forma se evita recargar la página
+    sendData(form);
 });
 
 const sendData = (data) => {
-  let xhr;
-  if(window.XMLHttpRequest) xhr = new XMLHttpRequest();
-  else xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    let xhr;
+    if(window.XMLHttpRequest) xhr = new XMLHttpRequest();
+    else xhr = new ActiveXObject("Microsoft.XMLHTTP");
 
-  xhr.open('POST', 'marvel.php');
-  const formData = new FormData(data); // Está es una función que puede recibir parámetros
-  xhr.send(formData);
+    xhr.open('POST', 'marvel.php');
+    const formData = new FormData(data); // Está es una función que puede recibir parámetros
+    xhr.send(formData);
 };
