0446915b6a90209cf3c5e88b373ce906e611828a
crisLeo <cristianleopaiva@gmail.com>
2020-08-16 20:22:24 -0300

    Try Catch

---
+++ 11 - Try Catch/script02.js
@@ -0,0 +1,21 @@
+﻿
+function color() {
+    let colorWeb = prompt('Introduce un color de la lista. Rojo/Verde/Azul').toLowerCase();
+
+    try {
+        if (colorWeb !== 'rojo' && colorWeb !== 'verde' && colorWeb !== 'azul') {
+            throw 'El color no esta contemplado'
+        }
+
+        if (colorWeb === 'rojo') {
+            cuerpo.style.background = '#ff354a';
+        } else if (colorWeb === 'verde') {
+            cuerpo.style.background = '#51ff66';
+        } else {
+            cuerpo.style.background = '#2345ff';
+        }
+
+    } catch (e) {
+        console.log("Error: " + e);
+    }
+}
