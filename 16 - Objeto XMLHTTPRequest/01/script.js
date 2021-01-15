430d76f727cb969cba91f1e70d3b0b9f4fa0b8da
crisLeo <cristianleopaiva@gmail.com>
2021-01-15 14:48:42 -0300

    Objeto XMLHttpRequest

--- 16 - Objeto XMLHTTPRequest/01/script.js
+++ 16 - Objeto XMLHTTPRequest/01/script.js
@@ -10,7 +10,6 @@ a una base de datos o estamos haciendo una consulta a una API que nos devuelve u
 es posible que la petición tarde y sobre t/ si tenemos una conexión de internet baja. Entonces lo que logra AJAX es que la página
 trabaje de forma asíncrona. Eso significa que no esta sincronizada con la página. Es decir, que la página va por un lado y las peticiones
 y los datos, van por otro.
-
  */
 
 /* Protocolo HTTP: https://es.wikipedia.org/wiki/Protocolo_de_transferencia_de_hipertexto */
