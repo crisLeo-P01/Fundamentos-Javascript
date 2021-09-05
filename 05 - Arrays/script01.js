c16248ab540565158c5afbb0a112b8c682df6499
crisLeo <cristianleopaiva@gmail.com>
2021-09-05 11:48:16 -0300

    Arrays

--- 05 - Arrays/script01.js
+++ 05 - Arrays/script01.js
@@ -4,19 +4,23 @@ document.write ("<p> El primer articulo es <strong>" + articulos [0] + "</strong
 
 document.write(`<p>El último elemento es <strong>${articulos[articulos.length-1]}</strong></p>`)
 
-articulos.unshift ("Balón"); //AGREGA ELEMENTO AL PRINCIPIO
+//AGREGA ELEMENTO AL PRINCIPIO
+articulos.unshift ("Balón");
 document.write(`<p>Casi nos olvidamos!!! Agregamos este articulo: <strong>${articulos[0]}</strong></p>`);
 
 document.write ("<p>Agreguemos un elemento más al final: <strong>");
-articulos.push (prompt ("Introduce el articulo")); //AGREGA ELEMENTO AL FINAL
+//AGREGA ELEMENTO AL FINAL
+articulos.push (prompt ("Introduce el articulo"));
 document.write (articulos[articulos.length-1] + "</strong></p>");
 
 document.write(`<p>${articulos[0]} <br> ${articulos[1]} <br> ${articulos[2]} <br> ${articulos[3]} <br> ${articulos[4]} <br> ${articulos[5]}</p>`);
 
 alert("Borraremos el primer elemento");
-articulos.shift (); //El método shift borra el primer elemento
+//El método shift borra el primer elemento
+articulos.shift ();
 document.write(`<p>${articulos[0]} <br> ${articulos[1]} <br> ${articulos[2]} <br> ${articulos[3]} <br> ${articulos[4]} <br> ${articulos[5]}</p>`);
 
 alert("Ahora borraremos el ultimo elemento");
-articulos.pop (); //El método pop borra el último elemento
+//El método pop borra el último elemento
+articulos.pop ();
 document.write(`<p>${articulos[0]} <br> ${articulos[1]} <br> ${articulos[2]} <br> ${articulos[3]}</p>`);
