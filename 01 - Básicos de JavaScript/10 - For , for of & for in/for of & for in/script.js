a058ee54bfac25afd6664272acd0f31e7a8d0c7e
crisLeo <cristianleopaiva@gmail.com>
2021-09-07 22:21:20 -0300

    Bucles FOR

--- 01 - Básicos de JavaScript/10 - For , for of & for in/for of & for in/script.js
+++ 01 - Básicos de JavaScript/10 - For , for of & for in/for of & for in/script.js
@@ -1,17 +1,47 @@
 
+// FOR IN /////////////////////////////////////////////
 
 let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-for(let i in dias) {
-	console.log(dias[i]);
+for (let i in dias) {
+	console.log (i);
 }
 
 /////////////////////////////////
 
-let nombres = [ "Cristian", "Male", "Emiliano", "Jesica", "Alejandro" ];
+let nombres = ["Cristian", "Male", "Emiliano", "Jesica", "Alejandro"];
 
-for ( let z in nombres ) {
+for (let z in nombres) {
 	if ( nombres[z] === "Emiliano" ) {
 		break;
 	}
 	alert (nombres[z]);
 }
+
+// FOR OF /////////////////////////////////////////////
+
+let numeros = [7, 13, 15, 32, 43, 107];
+for (let numero of numeros) {
+	console.log (numero)
+}
+
+/////////////////////////////////
+
+// Palabras reservadas de blucles => BREAK & CONTINUE /////////////////////////////////////////////
+
+//BREAK => rompe el bucle
+//CONTINUE => se salta la(s) posiciones que le indiquemos y despúes continúa su ejecución
+
+for (let dia in dias) {
+	if (dias[dia] === 'Jueves'){
+		break
+	}
+	console.log (dia)
+}
+
+for (let name of nombres) {
+	if (name === 'Emiliano') {
+		continue
+	}
+	console.log (name);
+}
+
