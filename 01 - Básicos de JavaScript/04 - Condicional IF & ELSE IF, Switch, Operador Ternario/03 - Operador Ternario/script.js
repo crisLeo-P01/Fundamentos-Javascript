48c36c6eb3c681789f46a0fd2213b39ad6989967
crisLeo <cristianleopaiva@gmail.com>
2021-09-09 00:48:50 -0300

    Operador Ternario

--- 01 - Básicos de JavaScript/04 - Condicional IF & ELSE IF, Switch, Operador Ternario/03 - Operador Ternario/script.js
+++ 01 - Básicos de JavaScript/04 - Condicional IF & ELSE IF, Switch, Operador Ternario/03 - Operador Ternario/script.js
@@ -2,9 +2,9 @@ let num = 5;
 let num3 = 23;
 
 // CON SOLO UNA SENTENCIA
-(num % 2 === 0) ? console.log(`${num} es par`) : console.log(`${num} es impar`);
+(num % 2 === 0) ? console.log (`${num} es par`) : console.log (`${num} es impar`);
 
-(num3 >= 18) ? document.write("eres mayor") : document.write("Eres menor");
+(num3 >= 18) ? document.write ("eres mayor") : document.write("Eres menor");
 
 // if (num % 2 == 0) console.log(`${num} es par`);
 // else console.log(`${num} es impar`);
@@ -12,10 +12,11 @@ let num3 = 23;
 let num2 = 8;
 
 // CON MÚLTIPLES SENTENCIAS
-(num2 % 2 === 0) ? (
-    console.log(`${num2} es par`),
-    console.log(`${num2} es par 2`)
+(num2 % 2 === 0) ? 
+    (
+        console.log (`${num2} es par`),
+        console.log (`${num2} es par 2`)
     ) : (
-        console.log(`${num2} es impar`),
-        console.log(`${num2} es par 2`)
+            console.log (`${num2} es impar`),
+            console.log (`${num2} es par 2`)
         )
