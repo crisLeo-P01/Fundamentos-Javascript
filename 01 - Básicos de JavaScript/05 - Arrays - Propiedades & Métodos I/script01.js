760ca80a2810807ed847db78fbd8e47124ce0a0b
crisLeo <cristianleopaiva@gmail.com>
2021-09-10 17:28:46 -0300

    Array - Métodos y propiedades

--- 01 - Básicos de JavaScript/05 - Arrays - Propiedades & Métodos I/script01.js
+++ 01 - Básicos de JavaScript/05 - Arrays - Propiedades & Métodos I/script01.js
@@ -1,6 +1,5 @@
-
 //PROPIEDADES ARRAY
-let numerosF = [1,2,3,4,5,6,7];
+let numerosF = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(numerosF.length);
 console.log(numerosF[3]);
@@ -18,20 +17,20 @@ console.log(Array.isArray(numerosF));
 
 let articulos = ["Zapatillas", "Camisetas", "Pantalon", "Calcetines"];
 //UNSHIFT agrega uno o mas elementos a lo primero
-articulos.unshift ("Balón");
-console.log (`<p>Casi nos olvidamos!!! Agregamos este articulo: <strong>${articulos[0]}</strong></p>`);
+articulos.unshift("Balón");
+console.log(`<p>Casi nos olvidamos!!! Agregamos este articulo: <strong>${articulos[0]}</strong></p>`);
 
 //PUSH agrega uno o mas elementos al array a lo último
-articulos.push (prompt ("Introduce el articulo"));
-console.log (articulos[articulos.length-1] + "</strong></p>");
+articulos.push(prompt("Introduce el articulo"));
+console.log(articulos[articulos.length-1] + "</strong></p>");
 
 //SHIFT elimina el primer elemento del array
-articulos.shift ();
+articulos.shift();
 console.log(`<p>${articulos[0]} <br> ${articulos[1]} <br> ${articulos[2]} <br> ${articulos[3]} <br> ${articulos[4]} <br> ${articulos[5]}</p>`);
 
 //POP elimina el último elemento del array
 //El método pop borra el último elemento
-articulos.pop ();
+articulos.pop();
 console.log(`<p>${articulos[0]} <br> ${articulos[1]} <br> ${articulos[2]} <br> ${articulos[3]}</p>`);
 
 ///////////////////////////////////////////
@@ -42,10 +41,10 @@ console.log(marcasAutos.reverse());
 
 ///////////////////////////////////////////
 //JOIN() devuelve un string con el separador que le indiquemos (por defecto, son comas)
-console.log(' JOIN()')
+console.log(' JOIN()');
 let numJoin = [4, 7, 14, 3, 32, 43];
 console.log(numJoin);
-console.log(numJoin.join(' / '))
+console.log(numJoin.join(' / '));
 
 ///////////////////////////////////////////
 /*
@@ -57,7 +56,7 @@ items - elementos a añadir en el caso de que se añadan(opcional)
 
 let numSplice = [5, 9, 14, 32, 75, 105];
 //numSplice.splice(2)
-numSplice.splice(2, 2)
+numSplice.splice(2, 2);
 // numSplice.splice(2, 0, 50, 51,52)
 console.log(numSplice);
 
