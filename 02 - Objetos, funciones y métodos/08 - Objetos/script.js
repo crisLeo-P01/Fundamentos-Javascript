0b22f70bf64efef856cbc05e34e822af205b8dfe
crisLeo <cristianleopaiva@gmail.com>
2021-09-11 23:44:23 -0300

    Objetos

--- 02 - Objetos, funciones y métodos/08 - Objetos/script.js
+++ 02 - Objetos, funciones y métodos/08 - Objetos/script.js
@@ -1,30 +1,31 @@
+/* eslint-disable guard-for-in */
 const persona = { //objeto
     nombre: "Cristian", // índice: valor;
     edad: 34,
     dni: 31700716,
     sexo: "Masculino",
-    deportesFavoritos: ['Fútbol' , 'Fórmula 1' , 'Tennis', 'NBA', 'UFC'],
+    deportesFavoritos: ['Fútbol', 'Fórmula 1', 'Tennis', 'NBA', 'UFC'],
 };
 
-console.log('Nombre del usuario: ' + persona.nombre); //console.log('Nombre del usuario: ' + persona.[' nombre']);
-console.log('Edad del usuario: ' + persona.edad);
-console.log('Los deportes favoritos del usuario son: ' + persona.deportesFavoritos);
+console.log(`Nombre del usuario: ${persona.nombre}`); //console.log('Nombre del usuario: ' + persona.['nombre']);
+console.log(`Edad del usuario: ${persona.edad}`);
+console.log(`Los deportes favoritos del usuario son: ${persona.deportesFavoritos}`);
 
-for(const z in persona) {
+for (const z in persona) {
     console.log(z); //Recorremos los índices del objeto
 }
 
-for(const i in persona) {
+for (const i in persona) {
     console.log(persona[i]); //Recorremos los valores del objeto
 }
 
-for(const depFav in persona.deportesFavoritos) {
+for (const depFav of persona.deportesFavoritos) {
     console.log(depFav); // Recorremos los valores de dichos indices
 }
+
 /*Si ponemos in en el bucle for nos recorre el índice del array. Para mostrar las claves
 de dicha array hay que colocar "of" y de esa forma nos dara todos los valores que
-contiene el array.
-*/
+contiene el array.*/
 
 // IMPRIMIENDO TODO EN UNA SOLA LÍNEA
 console.log(`El nombre es ${persona.nombre}. Su edad es de ${persona.edad} años. Sus deportes favoritos son: ${persona.deportesFavoritos.join(', ')}`);
