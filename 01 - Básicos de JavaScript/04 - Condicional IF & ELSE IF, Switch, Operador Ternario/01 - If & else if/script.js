2f7a73083c8b3eacc48c98739344ff8107f67911
crisLeo <cristianleopaiva@gmail.com>
2021-09-12 00:40:47 -0300

    Condicional IF & ELSE IF, Switch, Operador Ternario

--- 01 - Básicos de JavaScript/04 - Condicional IF & ELSE IF, Switch, Operador Ternario/01 - If & else if/script.js
+++ 01 - Básicos de JavaScript/04 - Condicional IF & ELSE IF, Switch, Operador Ternario/01 - If & else if/script.js
@@ -1,4 +1,3 @@
-
 let num = 6;
 
 /*Condicional simple*/
@@ -10,19 +9,18 @@ if (num > 0) {
 let num2 = 7;
 
 if (num2 < 5) {
-  console.log(`El número ${num2} es menor a 5`)
+  console.log(`El número ${num2} es menor a 5`);
 } else {
-  console.log(`El número ${num2} es mayor a 5`)
+  console.log(`El número ${num2} es mayor a 5`);
 }
 
 /*Condicional múltiples*/
 let num3 = 0;
 
 if (num3 > 0) {
-  console.log(`El número ${num3} es mayor a 0`)
+  console.log(`El número ${num3} es mayor a 0`);
 } else if (num3 < 0) {
-  console.log(`El número ${num3} es menor a 0`)
+  console.log(`El número ${num3} es menor a 0`);
 } else {
-  console.log(`El número ${num3} es igual a 0`)
+  console.log(`El número ${num3} es igual a 0`);
 }
-
