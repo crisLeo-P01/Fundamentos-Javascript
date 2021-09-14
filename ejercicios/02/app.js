a39c1ff5e65555f9cf2c12b29b9dd5d9fe079ce7
crisLeo <cristianleopaiva@gmail.com>
2021-09-13 23:15:02 -0300

    Ejercicios

--- ejercicios/02/app.js
+++ ejercicios/02/app.js
@@ -3,36 +3,36 @@
 ✔- La clase libro tendrá título, autor, año y género.
 ✔- Crea un método que devuelva toda la información del libro.
 ✔- Pide 3 libros y guárdalos en un array
-✔- Los libros se introducirán al arrancar el programa pidiendo los datos con 
+✔- Los libros se introducirán al arrancar el programa pidiendo los datos con
 prompt.
 ✔- Validar que los campos no se introduzcan vacíos.
 ✔- Validar que el año sea un número y que tenga 4 dígitos.
 ✔- Validar que el género sea: aventuras, terror o fantasía
 ✔- Crea una función que muestre todos los libros
 ✔- Crea una función que muestre los autores ordenados alfabéticamente
-- Crea una función que pida un género y muestre la información de los libros que 
+- Crea una función que pida un género y muestre la información de los libros que
 pertenezcan a ese género usando el método que devuelve la información
 */
 
 class Libro {
-	constructor(titulo, autor, anio, genero) {
-		this.titulo = titulo
-		this.autor = autor
-		this.anio = anio
-		this.genero = genero
-	}
+  constructor(titulo, autor, anio, genero) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anio = anio;
+    this.genero = genero;
+  }
 
-	getAutor() {
-  	return this.autor;
+  getAutor() {
+    return this.autor;
   }
 
   getGeneros() {
-  	return this.genero;
+    return this.genero;
   }
 
-	info() {
-		return `El título del libro es ${this.titulo} del autor ${this.autor}. Es del año ${this.anio}. Su género es ${this.genero}.`
-	} 
+  info() {
+    return `El título del libro es ${this.titulo} del autor ${this.autor}. Es del año ${this.anio}. Su género es ${this.genero}.`;
+  }
 }
 
 let iLibro;
@@ -43,54 +43,51 @@ let ingresarLibro = "si";
 let books = [];
 
 do {
-	iLibro = prompt('Introduzca el título del libro');
-	while(iLibro == "") {
-		iLibro = prompt('Por favor, introduzca el nombre del libro');
-	}
+  iLibro = prompt("Introduzca el título del libro");
+  while (iLibro === "") {
+    iLibro = prompt("Por favor, introduzca el nombre del libro");
+  }
 
-	iAutor = prompt('¿De quién pertenece dicho libro?');
-	while(iAutor == "") {
-		iAutor = prompt('Por favor, introduzca un nombre');
-	}
+  iAutor = prompt("¿De quién pertenece dicho libro?");
+  while (iAutor === "") {
+    iAutor = prompt("Por favor, introduzca un nombre");
+  }
 
-	iAnio = parseInt(prompt('Introduzca el año del libro'));
-	while(iAnio <= 0 || iAnio >2021) {
-		iAnio = parseInt(prompt('Por favor, introduzca una fecha válida'));
-	}
+  iAnio = parseInt(prompt("Introduzca el año del libro"));
+  while (iAnio <= 0 || iAnio > 2021) {
+    iAnio = parseInt(prompt("Por favor, introduzca una fecha válida"));
+  }
 
-	iGenero = prompt('introduce el género del libro. Deben ser de genero => Aventuras, terror o fantasía').toLowerCase();
-	while(iGenero == "" || iGenero !== "aventuras" && iGenero !== "aventura" && iGenero !== "terror" && iGenero !== "fantasía" && iGenero !== "fantasia") {
-		iGenero = prompt('Por favor, introduzca un nombre. Las opciones son => Aventuras, terror o fantasía');
-	}
+  iGenero = prompt("introduce el género del libro. Deben ser de genero => Aventuras, terror o fantasía").toLowerCase();
+  while (iGenero === "" || (iGenero !== "aventuras" && iGenero !== "aventura" && iGenero !== "terror" && iGenero !== "fantasía" && iGenero !== "fantasia")) {
+    iGenero = prompt("Por favor, introduzca un nombre. Las opciones son => Aventuras, terror o fantasía");
+  }
 
-	ingresarLibro = prompt('¿Desea ingresar un nuevo libro?');
+  ingresarLibro = prompt("¿Desea ingresar un nuevo libro?");
 
-	books.push(new Libro(iLibro, iAutor, iAnio, iGenero));
-
-
-}while(ingresarLibro == "si")
+  books.push(new Libro(iLibro, iAutor, iAnio, iGenero));
+} while (ingresarLibro === "si");
 
 console.log(books);
 
 const mostrarAutores = () => {
-	let authors = []
-	for (const book of books) {
-		authors.push(book.getAutor());
-	}
-	console.log(authors.sort());
-}
+  let authors = [];
+  for (const book of books) {
+    authors.push(book.getAutor());
+  }
+  console.log(authors.sort());
+};
 
 mostrarAutores();
 
 // PEDIRA QUE GÉNERO QUEREMOS MOSTRAR
 const mostrarGeneros = () => {
-  const gender = prompt('Introduce el género a buscar');
+  const gender = prompt("Introduce el género a buscar");
   for (const book of books) {
-    if (book.getGeneros() == gender) {
-    	console.log(book.info());
+    if (book.getGeneros() === gender) {
+      console.log(book.info());
     }
   }
-}
+};
 
 mostrarGeneros();
-
