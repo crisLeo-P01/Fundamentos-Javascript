701aac08fd16dfeb07ce7c136b118f28bd547d47
crisLeo-P01 <cristianleopaiva@gmail.com>
2021-11-03 00:50:01 -0300

    Axios

--- 04 - Peticiones HTTP/26 - Librería axios - GET & POST/script02.js
+++ 04 - Peticiones HTTP/26 - Librería axios - GET & POST/script02.js
@@ -1,24 +1,7 @@
 const button = document.getElementById('button');
 
-//res = response = respuesta
-/* button.addEventListener('click', () => {
-    const newPost = {
-      title: 'A new post',
-      body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      userId: 1
-    }
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify(newPost),
-      headers: {
-          "Content-type": "application/json"
-      }
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
-}) */
-
 button.addEventListener('click', () => {
+
   axios({
     method: 'POST',
     url: 'https://jsonplaceholder.typicode.com/posts',
@@ -27,6 +10,8 @@ button.addEventListener('click', () => {
         body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
         userId: 1
     }
-  }).then(res => console.log(res.data))
-      .catch(err => console.log(err))
+  })
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+
 });
