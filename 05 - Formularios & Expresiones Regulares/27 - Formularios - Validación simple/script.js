b71bf4f98cafec0fc1c066b0350063b1dc1701c5
crisLeo <cristianleopaiva@gmail.com>
2021-11-07 23:29:54 -0300

    Formularios & Expresiones Regulares

--- 05 - Formularios & Expresiones Regulares/27 - Formularios - Validación simple/script.js
+++ 05 - Formularios & Expresiones Regulares/27 - Formularios - Validación simple/script.js
@@ -6,38 +6,42 @@ const email = document.getElementById('email');
 const gender = document.getElementById('gender');
 const terms = document.getElementById('terms');
 
-const formInValid = { //Cuando la página esta cargada todos los items del form están en false
-    name: false,
-    email: false,
-    gender: false,
-    terms: false,
+const formInValid = { // Cuando la página esta cargada todos los items del form están en false
+  name: false,
+  email: false,
+  gender: false,
+  terms: false,
 };
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault(); //Es para prevenir que el formulario sea enviado sin ningún dato o sin completar algún campo
-    validateForm(); // Función que valide
+  e.preventDefault(); // Es para prevenir que el formulario sea enviado sin ningún dato o sin completar algún campo
+  validateForm(); // Función que valide
 });
 
-name.addEventListener('change', (e) => { //El change hace que detecte cualquier cambio dentro del campo
-    if(e.target.value.trim().length > 0) formInValid.name = true //trim() es para que no haya espacios en blanco
+name.addEventListener('change', (e) => { // El change hace que detecte cualquier cambio dentro del campo
+  if (e.target.value.trim().length > 0) formInValid.name = true; // trim() es para que no haya espacios en blanco
 });
 
 email.addEventListener('change', (e) => {
-    if(e.target.value.trim().length > 0) formInValid.email = true
+  if (e.target.value.trim().length > 0) formInValid.email = true;
 });
 
 gender.addEventListener('change', (e) => {
-    if(e.target.checked === true) formInValid.gender = true
+  if (e.target.checked === true) formInValid.gender = true;
 });
 
 terms.addEventListener('change', (e) => {
-    formInValid.terms = e.target.checked; //Esto es que si el checked es true el formInValid es true y sino lo contrario
-    e.target.checked ? button.removeAttribute('disabled') : button.setAttribute('disabled', true);
+  formInValid.terms = e.target.checked; // Esto es que si el checked es true el formInValid es true y sino lo contrario
+  e.target.checked ? button.removeAttribute('disabled') : button.setAttribute('disabled', true);
 });
 
 const validateForm = () => {
-    const formValues = Object.values(formInValid); //Como el objeto no nos sirve lo convertimos en un array para comprobar que no haya ningún false
-    const valid = formValues.findIndex(value => value === false); //Esto lo que hace es buscar si hay algún false. Si por ej en la posición de email es false no daría uno en la posición del array. Si da -1 es que email es true.
-    if(valid === -1) form.submit(); //En caso que sea true con -1, enviará el formulario.
-    else alert('Form Invalid');
+  // Como el objeto no nos sirve lo convertimos en un array para comprobar que no haya ningún false
+  const formValues = Object.values(formInValid);
+
+  /* Esto lo que hace es buscar si hay algún false. Si por ej en la posición de email es false no daría uno en la posición del array.
+  Si da -1 es que email es true. */
+  const valid = formValues.findIndex((value) => value === false);
+  if (valid === -1) form.submit(); // En caso que sea true con -1, enviará el formulario.
+  else alert('Form Invalid');
 };
