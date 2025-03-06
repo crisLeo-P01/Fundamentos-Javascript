be1fd932152c6401485612d04da9ca21a79b978e
Cristian Paiva <cristianleopaiva@gmail.com>
2025-03-06 16:55:40 -0300

    Modificando carpetas

--- 05 - Asincronía/25 - Async - await/script01.js
+++ 05 - Asincronía/25 - Async - await/script01.js
@@ -1,16 +1,16 @@
 // Asymc await funcionan a través de promesas
 
 const getName = async () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Cristian');
-    }, 1500)
-  });
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Cristian');
+        }, 1500)
+    });
 };
 
 const sayHello = async () => {
-  const name = await getName();
-  return `Hola ${name}`;
+    const name = await getName();
+    return `Hola ${name}`;
 };
 
 sayHello().then((res) => console.log(res));
--- 05 - Asincronía/25 - Async - await/script02.js
+++ 05 - Asincronía/25 - Async - await/script02.js
@@ -6,37 +6,37 @@ const users = [{ id: 1, name: 'Dorian' }, { id: 2, name: 'Laura' }, { id: 3, nam
 const emails = [{ id: 1, email: 'dorian@gmail.com' }, { id: 2, email: 'laura@gmail.com' }];
 
 const getUser = async (id) => { // Al escribir async ya estamos devolviendo una promesa.
-  const user = users.find((user) => user.id === id);
-  /* Lanzando un nuevo error con throw new Error, en el cual, el
-  new Error es un constructor que construye un objeto error y se le pasa
-  el mensaje de error.
-  Throw es una excepción o un error */
-  if (!user) throw new Error(`Doesn't exist an user with id ${id}`);
-  else return (user);
+    const user = users.find((user) => user.id === id);
+    /* Lanzando un nuevo error con throw new Error, en el cual, el
+    new Error es un constructor que construye un objeto error y se le pasa
+    el mensaje de error.
+    Throw es una excepción o un error */
+    if (!user) throw new Error(`Doesn't exist an user with id ${id}`);
+    else return (user);
 };
 
 const getEmail = async (user) => {
-  const email = emails.find((email) => email.id === user.id);
-  if (!email) throw new Error(`${user.name} hasn't email`);
-  else {
-    return ({
-      id: user.id,
-      name: user.name,
-      email: email.email,
-    });
-  }
+    const email = emails.find((email) => email.id === user.id);
+    if (!email) throw new Error(`${user.name} hasn't email`);
+    else {
+        return ({
+            id: user.id,
+            name: user.name,
+            email: email.email,
+        });
+    }
 };
 
 const getInfo = async (id) => {
-  /* La declaración try...catch señala un bloque de instrucciones a intentar (try), y especifica una respuesta si se
-  produce una excepción (catch). */
-  try {
-    const user = await getUser(id);
-    const res = await getEmail(user);
-    return `${user.name} email is ${res.email}`;
-  } catch (e) {
-    console.log(e);
-  }
+    /* La declaración try...catch señala un bloque de instrucciones a intentar (try), y especifica una respuesta si se
+    produce una excepción (catch). */
+    try {
+        const user = await getUser(id);
+        const res = await getEmail(user);
+        return `${user.name} email is ${res.email}`;
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 getInfo(1).then((res) => console.log(res));
